b5c2e51adbc50ab546e0cb46ffabff962a2d112a
EvgenyBelecky <enbel@mail.ru>
2022-12-15 12:30:08 +0300

    6. Идеальный вес [#41584 #371185]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,13 +2,7 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double dx = (x2 - x1);
-        double dxPow = Math.pow(dx, 2);
-        double dy = (y2 - y1);
-        double dyPow = Math.pow(dy, 2);
-        double sum = dxPow + dyPow;
-        double rsl = Math.sqrt(sum);
-        return  rsl;
+        return  Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     public static void main(String[] args) {
